10eb130aa84d560d4e31489a2b3fcf090078737c
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-06-04 16:45:14 -0400

    Create navigation bar

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { Stack, useRouter } from 'expo-router'
 
 import { COLORS, icons, images, SIZES } from '../constants'
-import { Nearbyjobs, Poularjobs,  ScreenHeaderBtn, Welcome } from '../components'
+import { Nearbyjobs, Popularjobs, Poularjobs,  ScreenHeaderBtn, Welcome } from '../components'
 
 
 const Home = () => {
@@ -13,9 +13,25 @@ const Home = () => {
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen 
                 options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite}
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn  iconUrl={icons.menu} dimension="60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn  iconUrl={images.profile} dimension="100%" />
+                    ),
+                    headerTitle: ""
                 }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex: 1, padding: SIZES.medium}}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
