88a251145d072da35bddcab1059192cd0ab22376
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-06-05 15:14:12 -0400

    Add tabs for job details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -7,6 +7,8 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from '../../constants'
 import useFetch from '../../hook/useFetch'
 
+const tabs = ["About", "Qualifications", "Responsibilities"]
+
 const JobDetails = () => {
     const params = useSearchParams();
     const router = useRouter();
@@ -14,6 +16,7 @@ const JobDetails = () => {
     const { data, isLoading, error, refetch } = useFetch('job-details', {job_id: params.id})
 
     const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setActiveTab] = useState(tabs[0])
 
     const onRefresh = () => {}
 
@@ -59,7 +62,9 @@ const JobDetails = () => {
                         />
 
                         <JobTabs 
-                        
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
                         />
 
                     </View>)
