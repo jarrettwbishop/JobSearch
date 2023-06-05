5f0db766a6c7ad7c42100864131d98c689552aa0
Jarrett Bishop <jarrettwbishop@gmail.com>
2023-06-05 15:51:48 -0400

    Create footer with 'Apply for job' button

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -94,6 +94,8 @@ const JobDetails = () => {
                     </View>)
                 }
             </ScrollView>
+
+            <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'}/>
         </>
     </SafeAreaView>
   )
